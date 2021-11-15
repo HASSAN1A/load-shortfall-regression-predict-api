36a438d72a3d45aaf039c9a93dc0b20b1d3f1bc7
Hassan  Juma <68612030+HASSAN1A@users.noreply.github.com>
2021-11-15 16:54:39 +0300

    Update model.py
    add best feature columns

--- model.py
+++ model.py
@@ -50,9 +50,7 @@ def _preprocess_data(data):
 
     
     # ----------- Replace this code with your own preprocessing steps --------
-    predict_vector = feature_vector_df[['Madrid_wind_speed', 'Seville_clouds_all', 'Bilbao_wind_deg',
-                                        'Seville_rain_1h', 'Barcelona_rain_3h', 'Valencia_snow_3h',
-                                        'Bilbao_pressure', 'Bilbao_weather_id', 'Valencia_temp_min']]
+    predict_vector = feature_vector_df[[['Day', 'Month', 'Year', 'Start_hour']  ]]
     return predict_vector
     
 def load_model(path_to_model: str):
